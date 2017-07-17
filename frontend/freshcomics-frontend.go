38fe79ffc11858ee16a73a30fcc1d616e0373344
Cian Johnston <johnstcn@gmail.com>
2017-07-17 12:08:29 +0100

    frontend mvp

---
+++ frontend/freshcomics-frontend.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/azer/snakecase"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+var db *sqlx.DB
+
+type comic struct {
+	Name      string
+	Title     string
+	URL       string
+	Published time.Time
+}
+
+func getDB() {
+	dsn := os.Getenv("DATABASE_URL")
+	db = sqlx.MustConnect("postgres", dsn)
+	fmt.Println("connected to database")
+	db.MapperFunc(snakecase.SnakeCase)
+}
+
+func getComics() (*[]comic, error) {
+	comics := make([]comic, 0)
+	stmt := `SELECT DISTINCT ON (site_defs.id) site_defs.name, site_updates.url, site_updates.title, site_updates.published
+    FROM site_defs INNER JOIN site_updates ON site_defs.id = site_updates.site_def_id
+    ORDER BY site_defs.id, site_updates.published DESC;`
+	err := db.Select(&comics, stmt)
+	if err != nil {
+		fmt.Println("error fetching latest comic list:", err)
+		return nil, err
+	}
+	return &comics, nil
+}
+
+func comicsHandler(resp http.ResponseWriter, req *http.Request) {
+	comics, _ := getComics()
+	enc := json.NewEncoder(resp)
+	enc.SetIndent("", "\t")
+	enc.Encode(comics)
+}
+
+func init() {
+	getDB()
+}
+
+func main() {
+	listenAddress := os.Getenv("HOSTPORT")
+	http.HandleFunc("/", comicsHandler)
+	http.ListenAndServe(listenAddress, http.DefaultServeMux)
+}
