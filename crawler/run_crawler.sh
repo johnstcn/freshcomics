28a491fd1359b847c02358b651c2aa3b05c86346
Cian Johnston <johnstcn@gmail.com>
2017-07-21 09:12:45 +0100

    small changes to dev runner script

--- crawler/run_crawler.sh
+++ crawler/run_crawler.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 export FRESHCOMICS_CRAWLER_DEBUG=true
 export FRESHCOMICS_CRAWLER_HOST=localhost
-export FRESHCOMICS_CRAWLER_PORT=3000
+export FRESHCOMICS_CRAWLER_PORT=3001
 export FRESHCOMICS_CRAWLER_DSN='host=localhost user=freshcomics password=freshcomics_password dbname=freshcomicsdb sslmode=disable'
+export FRESHCOMICS_CRAWLER_CHECKINTERVALSECS=60
+export FRESHCOMICS_CRAWLER_CRAWLDISPATCHSECS=1
+export FRESHCOMICS_CRAWLER_BACKOFF=1,1
 go run freshcomics-crawler.go
