31a525d819ca08e1b29d62fb20d0095172b47fc0
Cian Johnston <johnstcn@gmail.com>
2017-07-21 09:12:19 +0100

    do not init common config as values get overwritten

--- common/config/config.go
+++ common/config/config.go
@@ -1,26 +1,8 @@
 package config
 
-import (
-	"log"
-
-	"github.com/kelseyhightower/envconfig"
-)
-
 type Config struct {
 	Debug               bool 	`default:"false"`
 	Host 				string 	`default:"0.0.0.0"`
 	Port 				int 	`default:"3000"`
 	DSN                 string  `default:"host=localhost user=freshcomics password=freshcomics_password dbname=freshcomicsdb sslmode=disable"`
-}
-
-var Cfg Config
-
-func init() {
-	err := envconfig.Process("freshcomics_", &Cfg)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	log.Println("[config] Debug:", Cfg.Debug)
-	log.Println("[config] Host:", Cfg.Host)
-	log.Println("[config] Port:", Cfg.Port)
 }
