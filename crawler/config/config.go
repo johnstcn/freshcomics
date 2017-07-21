7bf67b876190a0b6e6bfd77948e5bd1513d6dce0
Cian Johnston <johnstcn@gmail.com>
2017-07-21 09:02:38 +0100

    abstract some config elements into a common package

--- crawler/config/config.go
+++ crawler/config/config.go
@@ -1,33 +1,30 @@
 package config
 
 import (
-	"os"
+	"log"
 
 	"github.com/kelseyhightower/envconfig"
 
-	"github.com/johnstcn/freshcomics/common/log"
+	"github.com/johnstcn/freshcomics/common/config"
 )
 
-type Config struct {
-	Host 				string 	`default:"localhost"`
-	Port 				int 	`default:"3000"`
+type CrawlerConfig struct {
+	config.Config
 	CheckIntervalSecs 	int 	`default:"600"`
 	CrawlDispatchSecs 	int 	`default:"10"`
-	DSN                 string  `default:"host=localhost user=freshcomics password=freshcomics_password dbname=freshcomicsdb sslmode=disable"`
 	Backoff				[]int	`default:"1,10,30"`
 }
 
-var Cfg Config
+var Cfg CrawlerConfig
 
 func init() {
 	err := envconfig.Process("freshcomics_crawler", &Cfg)
 	if err != nil {
-		log.Error(err.Error())
-		os.Exit(1)
+		log.Fatal(err.Error())
 	}
-	log.Info("[config] Host:", Cfg.Host)
-	log.Info("[config] Port:", Cfg.Port)
-	log.Info("[config] CheckIntervalSecs:", Cfg.CheckIntervalSecs)
-	log.Info("[config] CrawlDispatchSecs:", Cfg.CrawlDispatchSecs)
-	log.Info("[config] Backoff:", Cfg.Backoff)
+	log.Println("[config] Host:", Cfg.Host)
+	log.Println("[config] Port:", Cfg.Port)
+	log.Println("[config] CheckIntervalSecs:", Cfg.CheckIntervalSecs)
+	log.Println("[config] CrawlDispatchSecs:", Cfg.CrawlDispatchSecs)
+	log.Println("[config] Backoff:", Cfg.Backoff)
 }
