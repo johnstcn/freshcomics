5c88b607b2573e10a17f67e992424ce24b114789
Cian Johnston <johnstcn@gmail.com>
2017-12-26 16:04:59 +0000

    add unit tests for store.RecordClick()

--- internal/common/store/store_test.go
+++ internal/common/store/store_test.go
@@ -1,6 +1,7 @@
 package store
 
 import (
+	"fmt"
 	"net"
 	"time"
 	"testing"
@@ -9,6 +10,7 @@ import (
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/suite"
 	"gopkg.in/DATA-DOG/go-sqlmock.v1"
+	"database/sql/driver"
 )
 
 type StoreTestSuite struct {
@@ -24,7 +26,7 @@ type mockIPInfoer struct {
 }
 
 func (m *mockIPInfoer) GetIPInfo(addr net.IP) (GeoLoc, error) {
-	args := m.Called()
+	args := m.Called(addr)
 	return args.Get(0).(GeoLoc), args.Error(1)
 }
 
@@ -56,6 +58,93 @@ func (s *StoreTestSuite) TestGetComics_OK() {
 	s.NotNil(comics)
 	s.EqualValues("Test Comic", comics[0].Name)
 	s.NoError(err)
+}
+
+func (s *StoreTestSuite) TestGetComics_Err() {
+	s.mdb.ExpectQuery(`SELECT site_defs.name, site_defs.nsfw, site_updates.id, site_updates.title, site_updates.seen_at FROM site_updates JOIN site_defs ON \(site_updates.site_def_id = site_defs.id\) WHERE site_updates.id IN \( SELECT DISTINCT ON \(site_def_id\) id FROM site_updates ORDER BY site_def_id, seen_at DESC \) ORDER BY seen_at desc;`).WillReturnError(fmt.Errorf("some error"))
+	comics, err := s.store.GetComics()
+	s.Nil(comics)
+	s.EqualError(err, "some error")
+}
+
+func (s *StoreTestSuite) TestGetRedirectURL_OK() {
+	rows := sqlmock.NewRows([]string{"url"}).AddRow("http://example.com")
+	s.mdb.ExpectQuery(`SELECT site_updates.url FROM site_updates WHERE id = \$1`).WithArgs("12345").WillReturnRows(rows)
+	url, err := s.store.GetRedirectURL("12345")
+	s.NoError(err)
+	s.EqualValues("http://example.com", url)
+}
+
+func (s *StoreTestSuite) TestGetRedirectURL_Err() {
+	s.mdb.ExpectQuery(`SELECT site_updates.url FROM site_updates WHERE id = \$1`).WithArgs("12345").WillReturnError(fmt.Errorf("some error"))
+	url, err := s.store.GetRedirectURL("12345")
+	s.Zero(url)
+	s.EqualError(err, "some error")
+}
+
+func (s *StoreTestSuite) TestRecordClick_OK() {
+	ip := net.ParseIP("169.254.169.254")
+	s.mip.On("GetIPInfo", ip).Return(GeoLoc{
+		Country: "IE",
+		Region: "L",
+		City: "Dublin",
+	}, nil).Once()
+	s.mdb.ExpectBegin()
+	s.mdb.ExpectExec(`INSERT INTO comic_clicks \(update_id, country, region, city\) VALUES \(\$1, \$2, \$3, \$4\);`).WithArgs(12345, "IE", "L", "Dublin").WillReturnResult(driver.ResultNoRows)
+	s.mdb.ExpectCommit()
+	err := s.store.RecordClick("12345", ip)
+	s.NoError(err)
+}
+
+func (s *StoreTestSuite) TestRecordClick_InvalidUpdateID() {
+	err := s.store.RecordClick("notaninteger", nil)
+	s.EqualError(err, `strconv.Atoi: parsing "notaninteger": invalid syntax`)
+}
+
+func (s *StoreTestSuite) TestRecordClick_InvalidIP() {
+	ip := net.ParseIP("169.254.169.254")
+	s.mip.On("GetIPInfo", ip).Return(GeoLoc{}, fmt.Errorf("invalid IP")).Once()
+	err := s.store.RecordClick("12345", ip)
+	s.EqualError(err, "invalid IP")
+}
+
+func (s *StoreTestSuite) TestRecordClick_ErrBeginTx() {
+	ip := net.ParseIP("169.254.169.254")
+	s.mip.On("GetIPInfo", ip).Return(GeoLoc{
+		Country: "IE",
+		Region: "L",
+		City: "Dublin",
+	}, nil).Once()
+	s.mdb.ExpectBegin().WillReturnError(fmt.Errorf("some error"))
+	err := s.store.RecordClick("12345", ip)
+	s.EqualError(err, "some error")
+}
+
+func (s *StoreTestSuite) TestRecordClick_ErrExec() {
+	ip := net.ParseIP("169.254.169.254")
+	s.mip.On("GetIPInfo", ip).Return(GeoLoc{
+		Country: "IE",
+		Region: "L",
+		City: "Dublin",
+	}, nil).Once()
+	s.mdb.ExpectBegin()
+	s.mdb.ExpectExec(`INSERT INTO comic_clicks \(update_id, country, region, city\) VALUES \(\$1, \$2, \$3, \$4\);`).WithArgs(12345, "IE", "L", "Dublin").WillReturnError(fmt.Errorf("some error"))
+	err := s.store.RecordClick("12345", ip)
+	s.EqualError(err, "some error")
+}
+
+func (s *StoreTestSuite) TestRecordClick_ErrCommitTx() {
+	ip := net.ParseIP("169.254.169.254")
+	s.mip.On("GetIPInfo", ip).Return(GeoLoc{
+		Country: "IE",
+		Region: "L",
+		City: "Dublin",
+	}, nil).Once()
+	s.mdb.ExpectBegin()
+	s.mdb.ExpectExec(`INSERT INTO comic_clicks \(update_id, country, region, city\) VALUES \(\$1, \$2, \$3, \$4\);`).WithArgs(12345, "IE", "L", "Dublin").WillReturnResult(driver.ResultNoRows)
+	s.mdb.ExpectCommit().WillReturnError(fmt.Errorf("some error"))
+	err := s.store.RecordClick("12345", ip)
+	s.EqualError(err, "some error")
 }
 
 func TestStoreTestSuite(t *testing.T) {
