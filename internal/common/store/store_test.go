366057d94e303648225d6b468cb2715c18b1f66d
Cian Johnston <johnstcn@gmail.com>
2017-12-27 16:55:40 +0000

    add more unit tests

--- internal/common/store/store_test.go
+++ internal/common/store/store_test.go
@@ -3,8 +3,9 @@ package store
 import (
 	"fmt"
 	"net"
-	"time"
+	"regexp"
 	"testing"
+	"time"
 
 	"github.com/jmoiron/sqlx"
 	"github.com/stretchr/testify/mock"
@@ -53,15 +54,16 @@ func (s *StoreTestSuite) TearDownTest() {
 
 func (s *StoreTestSuite) TestGetComics_OK() {
 	rows := sqlmock.NewRows([]string{"name", "nsfw", "id", "title", "seen_at"}).AddRow("Test Comic", false, 1, "Test Title", s.now())
-	s.mdb.ExpectQuery(`SELECT site_defs.name, site_defs.nsfw, site_updates.id, site_updates.title, site_updates.seen_at FROM site_updates JOIN site_defs ON \(site_updates.site_def_id = site_defs.id\) WHERE site_updates.id IN \( SELECT DISTINCT ON \(site_def_id\) id FROM site_updates ORDER BY site_def_id, seen_at DESC \) ORDER BY seen_at desc;`).WillReturnRows(rows)
+	s.mdb.ExpectQuery(regexp.QuoteMeta(sqlGetComics)).WillReturnRows(rows)
 	comics, err := s.store.GetComics()
 	s.NotNil(comics)
+	s.Len(comics, 1)
 	s.EqualValues("Test Comic", comics[0].Name)
 	s.NoError(err)
 }
 
 func (s *StoreTestSuite) TestGetComics_Err() {
-	s.mdb.ExpectQuery(`SELECT site_defs.name, site_defs.nsfw, site_updates.id, site_updates.title, site_updates.seen_at FROM site_updates JOIN site_defs ON \(site_updates.site_def_id = site_defs.id\) WHERE site_updates.id IN \( SELECT DISTINCT ON \(site_def_id\) id FROM site_updates ORDER BY site_def_id, seen_at DESC \) ORDER BY seen_at desc;`).WillReturnError(fmt.Errorf("some error"))
+	s.mdb.ExpectQuery(regexp.QuoteMeta(sqlGetComics)).WillReturnError(fmt.Errorf("some error"))
 	comics, err := s.store.GetComics()
 	s.Nil(comics)
 	s.EqualError(err, "some error")
@@ -69,14 +71,14 @@ func (s *StoreTestSuite) TestGetComics_Err() {
 
 func (s *StoreTestSuite) TestGetRedirectURL_OK() {
 	rows := sqlmock.NewRows([]string{"url"}).AddRow("http://example.com")
-	s.mdb.ExpectQuery(`SELECT site_updates.url FROM site_updates WHERE id = \$1`).WithArgs("12345").WillReturnRows(rows)
+	s.mdb.ExpectQuery(regexp.QuoteMeta(sqlGetRedirectURL)).WithArgs("12345").WillReturnRows(rows)
 	url, err := s.store.GetRedirectURL("12345")
 	s.NoError(err)
 	s.EqualValues("http://example.com", url)
 }
 
 func (s *StoreTestSuite) TestGetRedirectURL_Err() {
-	s.mdb.ExpectQuery(`SELECT site_updates.url FROM site_updates WHERE id = \$1`).WithArgs("12345").WillReturnError(fmt.Errorf("some error"))
+	s.mdb.ExpectQuery(regexp.QuoteMeta(sqlGetRedirectURL)).WithArgs("12345").WillReturnError(fmt.Errorf("some error"))
 	url, err := s.store.GetRedirectURL("12345")
 	s.Zero(url)
 	s.EqualError(err, "some error")
@@ -90,21 +92,16 @@ func (s *StoreTestSuite) TestRecordClick_OK() {
 		City: "Dublin",
 	}, nil).Once()
 	s.mdb.ExpectBegin()
-	s.mdb.ExpectExec(`INSERT INTO comic_clicks \(update_id, country, region, city\) VALUES \(\$1, \$2, \$3, \$4\);`).WithArgs(12345, "IE", "L", "Dublin").WillReturnResult(driver.ResultNoRows)
+	s.mdb.ExpectExec(regexp.QuoteMeta(sqlRecordClick)).WithArgs(12345, "IE", "L", "Dublin").WillReturnResult(driver.ResultNoRows)
 	s.mdb.ExpectCommit()
-	err := s.store.RecordClick("12345", ip)
+	err := s.store.RecordClick(12345, ip)
 	s.NoError(err)
-}
-
-func (s *StoreTestSuite) TestRecordClick_InvalidUpdateID() {
-	err := s.store.RecordClick("notaninteger", nil)
-	s.EqualError(err, `strconv.Atoi: parsing "notaninteger": invalid syntax`)
 }
 
 func (s *StoreTestSuite) TestRecordClick_InvalidIP() {
 	ip := net.ParseIP("169.254.169.254")
 	s.mip.On("GetIPInfo", ip).Return(GeoLoc{}, fmt.Errorf("invalid IP")).Once()
-	err := s.store.RecordClick("12345", ip)
+	err := s.store.RecordClick(12345, ip)
 	s.EqualError(err, "invalid IP")
 }
 
@@ -116,7 +113,7 @@ func (s *StoreTestSuite) TestRecordClick_ErrBeginTx() {
 		City: "Dublin",
 	}, nil).Once()
 	s.mdb.ExpectBegin().WillReturnError(fmt.Errorf("some error"))
-	err := s.store.RecordClick("12345", ip)
+	err := s.store.RecordClick(12345, ip)
 	s.EqualError(err, "some error")
 }
 
@@ -128,8 +125,8 @@ func (s *StoreTestSuite) TestRecordClick_ErrExec() {
 		City: "Dublin",
 	}, nil).Once()
 	s.mdb.ExpectBegin()
-	s.mdb.ExpectExec(`INSERT INTO comic_clicks \(update_id, country, region, city\) VALUES \(\$1, \$2, \$3, \$4\);`).WithArgs(12345, "IE", "L", "Dublin").WillReturnError(fmt.Errorf("some error"))
-	err := s.store.RecordClick("12345", ip)
+	s.mdb.ExpectExec(regexp.QuoteMeta(sqlRecordClick)).WithArgs(12345, "IE", "L", "Dublin").WillReturnError(fmt.Errorf("some error"))
+	err := s.store.RecordClick(12345, ip)
 	s.EqualError(err, "some error")
 }
 
@@ -141,10 +138,32 @@ func (s *StoreTestSuite) TestRecordClick_ErrCommitTx() {
 		City: "Dublin",
 	}, nil).Once()
 	s.mdb.ExpectBegin()
-	s.mdb.ExpectExec(`INSERT INTO comic_clicks \(update_id, country, region, city\) VALUES \(\$1, \$2, \$3, \$4\);`).WithArgs(12345, "IE", "L", "Dublin").WillReturnResult(driver.ResultNoRows)
+	s.mdb.ExpectExec(regexp.QuoteMeta(sqlRecordClick)).WithArgs(12345, "IE", "L", "Dublin").WillReturnResult(driver.ResultNoRows)
 	s.mdb.ExpectCommit().WillReturnError(fmt.Errorf("some error"))
-	err := s.store.RecordClick("12345", ip)
+	err := s.store.RecordClick(12345, ip)
 	s.EqualError(err, "some error")
+}
+
+func (s *StoreTestSuite) TestCreateSiteDef_OK() {
+	sd := SiteDef{
+		Name: "Test Name",
+		Active: true,
+		NSFW: true,
+		StartURL: "Test Start URL",
+		LastCheckedAt: s.now(),
+		URLTemplate: "Test Template",
+		RefXpath: "Test Ref XPath",
+		RefRegexp: "Test Ref Regexp",
+		TitleXpath: "Test Title XPath",
+		TitleRegexp: "Test Title Regexp",
+	}
+	rows := sqlmock.NewRows([]string{"id"}).AddRow(1)
+	s.mdb.ExpectBegin()
+	s.mdb.ExpectQuery(regexp.QuoteMeta(sqlCreateSiteDef)).WithArgs(sd.Name, sd.Active, sd.NSFW, sd.StartURL, sd.LastCheckedAt, sd.URLTemplate, sd.RefXpath, sd.RefRegexp, sd.TitleXpath, sd.TitleRegexp).WillReturnRows(rows)
+	s.mdb.ExpectCommit()
+	newid, err := s.store.CreateSiteDef(sd)
+	s.NotZero(newid)
+	s.NoError(err)
 }
 
 func TestStoreTestSuite(t *testing.T) {
