551e77b9ea942e65079d21c2fc391f0fbf15f366
Cian Johnston <public@cianjohnston.ie>
2019-02-24 08:51:34 +0000

    add dummy ipinfoer

---
+++ internal/ipinfo/dummy.go
@@ -0,0 +1,10 @@
+package ipinfo
+
+import "net"
+
+// DummyIPInfoer implements IPInfoer but actually does nothing
+type DummyLookuper struct {}
+
+func (l *DummyLookuper) Lookup(addr net.IP, result interface{}) error {
+	return nil
+}
