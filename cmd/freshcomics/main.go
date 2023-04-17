473f697dd7a2bc81a3f6d6ff73dc29be73cb1d8a
Cian Johnston <public@cianjohnston.ie>
2023-04-17 21:25:44 +0100

    rm cobra

--- cmd/freshcomics/main.go
+++ cmd/freshcomics/main.go
@@ -1,5 +1,69 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/jmoiron/sqlx"
+	"golang.org/x/exp/slog"
+
+	"github.com/johnstcn/freshcomics/internal/api"
+	"github.com/johnstcn/freshcomics/internal/store"
+)
+
 func main() {
-	root().Execute()
+	var (
+		host string
+		port int
+		dsn  string
+		log  = slog.New(slog.NewTextHandler(os.Stdout))
+	)
+
+	flag.StringVar(&host, "host", "0.0.0.0", "listen on this host")
+	if val, ok := os.LookupEnv("FRESHCOMICS_HOST"); ok {
+		host = val
+	}
+
+	flag.IntVar(&port, "port", 8000, "listen on this port")
+	if val, ok := os.LookupEnv("FRESHCOMICS_PORT"); ok {
+		if intVal, err := strconv.Atoi(val); err != nil {
+			log.Error("invalid port env", "val", val)
+			os.Exit(1)
+		} else {
+			port = intVal
+		}
+	}
+
+	flag.StringVar(&dsn, "dsn", "postgresql://localhost:5432/freshcomics?user=freshcomics&password=freshcomics", "postgresql connection string")
+	if val, ok := os.LookupEnv("FRESHCOMICS_DB"); ok {
+		dsn = val
+	}
+
+	conn, err := sqlx.Connect("postgres", dsn)
+	if err != nil {
+		log.Error("connect to db", "err", err)
+		os.Exit(1)
+	}
+
+	store, err := store.NewPGStore(conn)
+	if err != nil {
+		log.Error("init store", "err", err)
+		os.Exit(1)
+	}
+
+	listenAddress := fmt.Sprintf("%s:%d", host, port)
+	mux := http.NewServeMux()
+	srv := api.New(api.Deps{
+		Mux:    mux,
+		Store:  store,
+		Logger: log,
+	})
+
+	slog.Info("listen", "host", host, "port", port)
+	if err := http.ListenAndServe(listenAddress, srv); err != nil {
+		log.Error("listen and serve", "err", err)
+	}
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/johnstcn/freshcomics
 go 1.16
 
 require (
-	github.com/dustin/go-humanize v1.0.0
 	github.com/fiorix/freegeoip v3.4.1+incompatible
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/golang/mock v1.6.0
@@ -17,7 +16,6 @@ require (
 	github.com/oschwald/maxminddb-golang v1.8.0 // indirect
 	github.com/pkg/errors v0.9.1
 	github.com/sirupsen/logrus v1.8.1
-	github.com/spf13/cobra v1.7.0
 	github.com/stretchr/testify v1.8.1
 	golang.org/x/exp v0.0.0-20230321023759-10a507213a29
 	golang.org/x/net v0.7.0 // indirect
